e2e7f7a1ca5082021a15d5968ec9c0b5ead55085
Olga <helga_ser@mail.ru>
2023-08-01 22:24:05 +0300

    Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,6 @@
 public class CreditPaymentService {
     public int calculate(int credit, int period, float percent) {
-        float mPercent = (percent / 12) /100; // ежемесячная ставка
-        return (int) (credit * ((mPercent * (Math.pow((1+mPercent),period)))  / ((Math.pow((1+mPercent),period))-1)));
-
+        float mPercent = (percent / 12) / 100; // ежемесячная ставка
+        return (int) (credit * ((mPercent * (Math.pow((1 + mPercent), period))) / ((Math.pow((1 + mPercent), period)) - 1)));
     }
 }
